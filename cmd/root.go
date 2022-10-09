68be96e694d132ab80b64ecdf6ce14ddc912443c
Mike Rosset <mike.rosset@gmail.com>
2022-10-09 05:32:15 -0700

    root: use ShortName in commented config global flag

--- cmd/root.go
+++ cmd/root.go
@@ -77,7 +77,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.raijin.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.{{.ShortName}}.yaml)")
 	rootCmd.PersistentFlags().String("prefix", filepath.Join(home, "bitcoin"), "Directory Bitcoin Core is to be installed")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
