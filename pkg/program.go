e25511c0ec9396e988df7b316a6d58a7114be7ef
Mike Rosset <mike.rosset@gmail.com>
2022-10-09 17:39:40 -0700

    copyright: headers now used zeus as program name

--- pkg/program.go
+++ pkg/program.go
@@ -1,14 +1,14 @@
 /*
 Copyright © 2022 Michael Rosset <mike.rosset@gmail.com>
 
-# This file is part of Raijin
+# This file is part of Zeus
 
-Raijin is free software: you can redistribute it and/or modify it
+Zeus is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Raijin is distributed in the hope that it will be useful, but
+Zeus is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
